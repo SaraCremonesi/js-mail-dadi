61d2a452778e1d3207d329ad011db6f294c99def
SaraCremonesi <saracrem00@gmail.com>
2020-05-25 17:41:07 +0200

    completamento esercizio mail

--- script.js
+++ script.js
@@ -3,39 +3,39 @@
 var listaMail = ['giuseppe@gmail.com' , 'marco@gmail.com' , 'mauro@gmail.com' , 'paolo@gmail.com' , 'gianni@gmail.com' , 'andrea@gmail.com' , 'luca@gmail.com' , 'simone@gmail.com' , 'giulio@gmail.com'];
 
 // Inizializzo le variabili ch mi occorrono per la funzione click
-var mail =  document.getElementById('mail');
 var accedi = document.getElementById('accedi');
 var messaggio = document.getElementById('messaggio');
 var autorizzato = false;
 
 accedi.addEventListener('click',
   function () {
+    var mail =  document.getElementById('mail');
     for (i = 0; i < listaMail.length; i++) {
-      if (listaMail[i] === mail) {
+      if (listaMail[i] === mail.value) {
         autorizzato = true;
       }
     }
-    
-    if (autorizzato = true) {
+
+    if (autorizzato === true) {
       messaggio.innerHTML = 'ACCESSO AUTORIZZATO';
     } else {
       messaggio.innerHTML = 'ACCESSO NEGATO';
     }
   }
 );
-
 // FINE ESERCIZIO MAIL
 
 // INIZIO ESERCIZIO DADI
 // Inizializzo le variabili per l'assegnazione di valori casuali
-var randomUser = document.getElementById('random-user');
-var randomComputer = document.getElementById('random-pc');
+
 var generaButton = document.getElementById('genera');
 
 generaButton.addEventListener('click',
   function() {
-    numeroRandomUser = Math.floor(Math.random() * 6) + 1;
-    numeroRandomComputer = Math.floor(Math.random() * 6) + 1;
+  var randomUser = document.getElementById('random-user');
+  var randomComputer = document.getElementById('random-pc');
+  var numeroRandomUser = Math.ceil(Math.random() * 6) + 1;
+  var numeroRandomComputer = Math.ceil(Math.random() * 6) + 1;
 
     randomUser.innerHTML = numeroRandomUser.value;
     randomComputer.innerHTML = numeroRandomComputer.value;
