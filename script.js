fa62bc4cc232d897c7a5c3e047383c7b0fc88ce2
SaraCremonesi <saracrem00@gmail.com>
2020-05-25 16:12:32 +0200

    esercizio mail

---
+++ script.js
@@ -0,0 +1,20 @@
+// INIZIO ESERCIZIO MAIL
+// Creo un array con la lista delle mail delle persone autorizzate ad accedere
+var listaMail = ['giuseppe@gmail.com' , 'marco@gmail.com' , 'mauro@gmail.com' , 'paolo@gmail.com' , 'gianni@gmail.com' , 'andrea@gmail.com' , 'luca@gmail.com' , 'simone@gmail.com' , 'giulio@gmail.com'];
+
+// Inizializzo le variabili ch mi occorrono per la funzione click
+var mail =  document.getElementById('mail');
+var accedi = document.getElementById('accedi');
+
+accedi.addEventListener('click',
+  function () {
+    if (mail.value = ) {
+
+    }
+  }
+);
+
+// FINE ESERCIZIO MAIL
+
+// INIZIO ESERCIZIO DADI
+// FINE ESERCIZIO DADI
