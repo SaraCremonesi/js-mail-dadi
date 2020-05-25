f449d1fad95ae54d51e14cd99cb42c6ecf3ee725
SaraCremonesi <saracrem00@gmail.com>
2020-05-25 16:54:58 +0200

    aggiornamento

--- script.js
+++ script.js
@@ -5,11 +5,16 @@ var listaMail = ['giuseppe@gmail.com' , 'marco@gmail.com' , 'mauro@gmail.com' , 
 // Inizializzo le variabili ch mi occorrono per la funzione click
 var mail =  document.getElementById('mail');
 var accedi = document.getElementById('accedi');
+var messaggio = document.getElementById('messaggio');
 
 accedi.addEventListener('click',
   function () {
-    if (mail.value = ) {
-
+    for (i = 0; i < listaMail.length; i++) {
+      if (listaMail[i] === mail.value) {
+        messaggio.innerHTML = 'ACCESSO AUTORIZZATO';
+      } else {
+        messaggio.innerHTML = 'ACCESSO NEGATO';
+      }
     }
   }
 );
@@ -17,4 +22,18 @@ accedi.addEventListener('click',
 // FINE ESERCIZIO MAIL
 
 // INIZIO ESERCIZIO DADI
+// Inizializzo le variabili per l'assegnazione di valori casuali
+var randomUser = document.getElementById('random-user');
+var randomComputer = document.getElementById('random-pc');
+var generaButton = document.getElementById('genera');
+
+generaButton.addEventListener('click',
+  function() {
+    numeroRandomUser = Math.floor(Math.random() * 6) + 1;
+    numeroRandomComputer = Math.floor(Math.random() * 6) + 1;
+
+    randomUser.innerHTML = numeroRandomUser.value;
+    randomComputer.innerHTML = numeroRandomComputer.value;
+  }
+);
 // FINE ESERCIZIO DADI
